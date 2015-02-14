1f74eec89c2f517108c8d0b9fbb2b572286618c8
qinghao <qingxianhao@gmail.com>
2015-02-15 01:49:12 +0800

    tutorial part4 first half

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from polls.models import Poll
-from django.template import Context,loader
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import Context, loader
 from django.http import Http404
+from django.core.urlresolvers import reverse
+
+from polls.models import Question, Choice
+
 def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
-def detail(request, poll_id):
+    return render(request,
+                  'polls/index.html',
+                  {'latest_question_list': Question.objects.order_by('-pub_date')})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
     try:
-            poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-            raise Http404
-    return render(request, 'polls/detail.html', {'poll': poll})
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'question': question})
