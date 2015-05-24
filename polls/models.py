cea95b3c835f0a8e696ba309655416eeb92497e6
qinghao <qingxianhao@gmail.com>
2015-05-25 02:56:26 +0800

    cookie check

--- polls/models.py
+++ polls/models.py
@@ -19,9 +19,7 @@ class Member(models.Model):
     name   = models.CharField(max_length=200)
     emp_id = models.CharField(max_length=10) #N22270
     email  = models.EmailField()
-    ip = models.GenericIPAddressField(protocol='IPV4')
 
 class Vote(models.Model):
     question = models.ForeignKey(Choice)
     member_id = models.ForeignKey(Member)
-    
--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.core.urlresolvers import reverse
 
 from polls.models import Question, Choice
+import datetime
 
 def index(request):
     return render(request,
@@ -15,7 +16,20 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
+def has_voted(request):
+    return request.session.get('voted', 0)
+#return 0
+
+def register(request):
+    return 'register page'
+
 def vote(request, question_id):
+    output = ''
+    #for meta in request.META:
+    #    line = "%s == \"%s\"<br/>\n" % (meta, request.META[meta])
+    #    output += line
+    #return HttpResponse(output)
+
     p = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
@@ -26,12 +40,23 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        voted = has_voted(request)
+        if not voted:
+            selected_choice.votes += 1
+            selected_choice.save()
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            request.session['voted'] = 1
+            request.session.set_expiry(datetime.timedelta(days=7).seconds)
+            return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        elif voted == -1:
+            return HttpResponseRedirect(reverse('polls:register', args=(p.id,)))
+        else: # voded == 1
+            return render(request, 'polls/results.html', {
+                'question': p,
+                'error_message': "You have voted.",
+            })
 
 def detail(request, question_id):
     try:
